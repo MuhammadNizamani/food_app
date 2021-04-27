2c645885b59dea982cb9cb6cb6bebde2ad88af95
MuhammadIshaque <ishaque.niz786@gmail.com>
2021-04-28 00:26:05 +0500

    whole project

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:  "PakMeli",
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme:  ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(
+            color : Color.fromRGBO(20, 51, 51, 1)
+          ),
+          bodyText2: TextStyle(
+            color : Color.fromRGBO(20, 51, 51, 1)
+          ),
+          headline6: TextStyle(
+            fontSize: 20,
+            fontFamily: "RobotoCondensed",
+            fontWeight: FontWeight.bold
+          )
+
+        )
+      ),
+      home: CategoriesScreen(),
+
+    );
+    }
+    }
+      
