8b6a65d970d88ec79c5055da02bcdabc7a3586ab
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2021-05-06 16:16:47 +0500

    adding image inmeal detail

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeNmae = '/meal-detail';
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)!.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      body: Center(child: Text(" The Meal ! $mealId")),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
